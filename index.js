b4fade25aea5c7c5cfffb841761924dc5e154c9c
AyaMidani <61182291+AyaMidani@users.noreply.github.com>
2022-04-18 03:09:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(number) {
+    if(number<42)
+    {
+        return 42-number
+    }
+    else{
+        return number-42;
+    }
+  }
+
+function distanceFromHqInFeet(value){
+    return distanceFromHqInBlocks(value)*264;
+  }
+
+function distanceTravelledInFeet(start, destination) {
+    if(destination>start)
+    {
+        return (destination-start)*264
+    }
+    else{
+        return (start-destination)*264
+    }
+  }
+  function calculatesFarePrice(start, destination) {
+    const Feetcalcuates = distanceTravelledInFeet(start,destination);
+    if (Feetcalcuates<= 400){
+      return 0;
+    }
+    if (Feetcalcuates>=400 && Feetcalcuates<=2000){
+      return (Feetcalcuates-400)* 0.02;
+    }
+    else if(Feetcalcuates>=2000&& Feetcalcuates<=2500){
+      return 25;
+    }
+    else if (Feetcalcuates > 2500){
+      return "cannot travel that far";
+    }
+  }
+  distanceFromHqInBlocks(50)
+  distanceFromHqInFeet(50)
